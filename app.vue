2a3e87676c9dc32659567b8005e5469e2d270ca3
Gustavo Soeiro Senise <gustavosenise@gmail.com>
2025-07-08 14:54:11 -0300

    tailwind daisy and icons

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex items-center justify-center h-screen bg-center bg-cover bg-blend-overlay">  
+    <h1 class="text-8xl font-bold">Nuxt Map Log</h1>
+    <button class="btn btn-primary m-4" >
+        View Map <icon name="material-symbols:map-outline"></icon>
+      </button>
   </div>
 </template>
