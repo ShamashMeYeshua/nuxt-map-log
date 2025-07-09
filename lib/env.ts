9ef1aa3a6acc7c3f34d2e86b04348722d994523c
Gustavo Soeiro Senise <gustavosenise@gmail.com>
2025-07-09 14:49:22 -0300

    Auth OK

--- lib/env.ts
+++ lib/env.ts
@@ -4,7 +4,9 @@ import tryParseEnv from './try-parse-env';
 const envSchema = z.object({
     NODE_ENV: z.string(),
     TURSO_DATABASE_URL: z.string(),
-    TURSO_AUTH_TOKEN: z.string()
+    TURSO_AUTH_TOKEN: z.string(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string().url(),
 });
 
 export type EnvSchema = z.infer<typeof envSchema>;
