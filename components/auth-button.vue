88a25cdbe781fa067bee3022bbf4dc0a3d526c06
Gustavo Soeiro Senise <gustavosenise@gmail.com>
2025-07-10 13:38:14 -0300

    Fixes #17 Fixes #18

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -4,7 +4,24 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-    <button 
+    <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end dropdown-hover">
+        <div tabindex="0" role="button" class="btn m-1">
+            <div v-if="authStore.user.image" class="avatar">
+                <div class="w-8 rounded-xl">
+                    <img :src="authStore.user.image" :alt="authStore.user.name || authStore.user.email"/>
+                </div>
+            </div>
+            {{ authStore.user.name || authStore.user.email }}
+        </div>
+        <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li><NuxtLink to="/sign-out">
+                <Icon name="tabler:logout-2" size="24" />
+                Sign Out
+                
+            </NuxtLink></li>
+        </ul>
+    </div>
+    <button v-else
         :disabled="authStore.loading" 
         @click="authStore.signIn" 
         class="btn btn-accent">Sign In with Github
--- stores/auth.ts
+++ stores/auth.ts
@@ -1,24 +1,30 @@
 
-import { createAuthClient } from "better-auth/client";
+import { createAuthClient } from "better-auth/vue";
 
 const authClient =  createAuthClient()
 
 export const useAuthStore = defineStore('useAuthStore', () => {
+    const session = authClient.useSession();
+    const user = computed(() => session.value.data?.user);
+    const loading = computed(() => session.value.isPending || session.value.isRefetching);
 
-  const loading = ref(false);
+    async function signIn() {
+        await authClient.signIn.social({
+            provider: "github",
+            callbackURL: "/dashboard",
+            errorCallbackURL: "/error",
+        });
+    }
 
-  async function signIn() {
-      loading.value = true;
-      await authClient.signIn.social({
-          provider: "github",
-          callbackURL: "/dashboard",
-          errorCallbackURL: "/error",
-      });
-      loading.value = false;
-  }
+    async function signOut() {
+        await authClient.signOut();
+        navigateTo('/'); // Redirect to home page after sign out
+    }
 
-  return {
-      loading,
-      signIn
+    return {
+        loading,
+        signIn,
+        signOut,
+        user
   }
 })
