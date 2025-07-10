bcd3405d6f6e0d008aa43d9d20decde25adef682
Gustavo Soeiro Senise <gustavosenise@gmail.com>
2025-07-10 18:58:02 -0300

    SSR ok

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,31 @@
 import { betterAuth } from "better-auth";
-
+import { createAuthMiddleware } from "better-auth/plugins";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "./db/index"; 
 import env from "./env";
 
 export const auth = betterAuth({
+    hooks: {
+        after: createAuthMiddleware(async (ctx) => {
+            if (ctx.path === "/get-session") {
+                if (!ctx.context.session) {
+                    return ctx.json({
+                        session: null,
+                        user: null,
+                    });
+                }
+                return ctx.json(ctx.context.session);
+            }
+        }),
+    },
     database: drizzleAdapter(db, {
         provider: "sqlite", // or "mysql", "sqlite"
     }),
     advanced: {
-        generateId: false,
+        database: {
+            generateId: false, // Disable automatic ID generation
+        },
+        //generateId: false,
     },
     socialProviders: {
         github: { 
